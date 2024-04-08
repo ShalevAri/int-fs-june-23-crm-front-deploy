28e4930f87ec4880990081f78337e9287e2ed1b4
shalev <shalevari2008@gmail.com>
2024-04-08 18:29:09 +0300

    feat(app-page): format file and add temporary link
    This is so that until I fix the routing issue we can still navigate through routes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
 
-import "./App.scss";
+import "./App.scss"
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -19,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -29,8 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h2>Custom Routes</h2>
+      <a href="/registerLead">Register Lead</a>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
